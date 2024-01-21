5f3c79c3d1d909e12b39f619be203150db89df53
Fabrizio Ravelli <frz.ravelli@gmail.com>
2024-01-21 19:04:55 +0100

    added Extra ES

---
+++ ESERCITAZIONI/ES9_ControlloDiMoto.m
@@ -0,0 +1,75 @@
+%% Main
+% Inizializzazione del workspace
+clear
+close all
+clc
+
+% Variabili temporali di simulazione
+simulationTime = 5;
+samplingTime = 1e-2;
+t = 0 : samplingTime : simulationTime;
+nPoints = length(t);
+
+% Matrici di salvataggio
+position = zeros(nPoints,1);
+velocity = zeros(nPoints,1);
+acceleration = zeros(nPoints,1);
+
+posErr = zeros(nPoints,1);
+velErr = zeros(nPoints,1);
+
+% Stato iniziale
+position(1) = 0;
+velocity(1) = 0;
+acceleration(1) = 0;
+
+% Guadagni
+KP = 10;
+KV = 10;
+
+% Stima parametri dinamici e posizione desiderata
+xd = 0.2;
+xDotd = 0;
+xDDotd = 0;
+
+mHat = 5;
+m = 3;
+
+for i = 1 : nPoints
+
+    currentTime = t(i);
+
+    posErr(i) = xd - position(i);
+    velErr(i) = xDotd - velocity(i);
+
+    u = mHat*(xDDotd + KV*velErr(i) + KP*posErr(i));
+
+    if i < nPoints
+        acceleration(i+1) = u/m;
+        velocity(i+1) = velocity(i) + samplingTime*acceleration(i+1);
+        position(i+1) = position(i) + samplingTime*velocity(i+1);
+    end
+
+end
+
+%% Plot errori
+figure
+subplot 211
+plot(t, posErr, LineWidth=1.4)
+xlabel("Tempo [s]")
+ylabel("Errore [m]")
+title("Errore di posizione")
+grid on
+subplot 212
+plot(t, velErr, LineWidth=1.4)
+xlabel("Tempo [s]")
+ylabel("Errore [m/s]")
+title("Errore di velocità")
+grid on
+
+
+
+
+
+
+
